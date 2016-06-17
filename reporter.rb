1016a045678148bb38e30de49dd8296c15586f22
izumin5210 <masayuki@izumin.info>
2016-06-17 21:54:30 +0900

    Refactor

--- reporter.rb
+++ reporter.rb
@@ -15,41 +15,37 @@ Slack.configure do |config|
   config.token = SLACK_ACCESS_TOKEN
 end
 
+def format_body(text, file_comments)
+  URI.extract(text, ["http", "https"]).inject(text) do |md, url|
+    pattern = /uploaded a file: \<(?<url>#{url})\|(?<title>.+)\>(:? and commented: ?(?<comment>.+)\n)?/
+    replaced = md.gsub(pattern, '\k<title>: \k<url>')
+    unless Regexp.last_match.nil?
+      comment = Regexp.last_match[:comment]
+      file_comments[url].unshift(comment) if !comment.nil? && !comment.empty? && !(/^\s*$/ =~ comment)
+      replaced += "\n\n" + file_comments[url].map { |c| "- #{c}" }.join("\n")
+    end
+    replaced
+  end
+end
+
 SLACK_TARGET_CHANNELS.split(';').each do |channel_name|
   target_date = Date.today - 1
   hitorigoto_list = Hitorigoto.fetch(channel_name, target_date)
   unless hitorigoto_list.empty?
-    img_comments = Hash.new { |h, k| h[k] = [] }
+    file_comments = Hash.new { |h, k| h[k] = [] }
+
+    hitorigoto_list.reject! do |h|
+      pattern = /commented on \<.+\>'s file \<(?<url>.+)\|.+\>: (?<comment>.+)/
+      m = pattern.match(h.text)
+      unless m.nil?
+        file_comments[m[:url]] << "#{m[:comment]} by #{h.username} at [#{h.created_at.strftime('%T')}](#{h.permalink})"
+      end
+      !m.nil?
+    end
+
     body_md = hitorigoto_list
       .reverse
-      .map { |h| "### #{h.username} at [#{h.created_at.strftime('%T')}](#{h.permalink})\n#{h.text}" }
-      .map { |md_raw|
-        URI.extract(md_raw, ["http", "https"])
-          .inject(md_raw) { |md, url|
-            pattern = /uploaded a file: \<(?<url>#{url})\|(?<title>.+)\> and commented: ?(?<comment>.+)\n/
-            replaced = md_raw.gsub(pattern, '\k<title>: \k<url>\k<comment><COMMENTS|\k<url>>')
-            unless Regexp.last_match.nil?
-              comment = Regexp.last_match[:comment]
-              img_comments[url] << comment if !comment.empty? && !(/^\s*$/ =~ comment)
-            end
-            replaced
-          }
-      }
-      .reject { |md|
-        pattern = /commented on \<.+\>'s file \<(?<url>.+)\|.+\>: (?<comment>.+)/
-        m = pattern.match(md)
-        img_comments[m[:url]] << m[:comment] unless m.nil?
-        !m.nil?
-      }
-      .map { |md_raw|
-        pattern = /\<COMMENTS\|(?<url>.+)\>/
-        m = pattern.match(md_raw)
-        md = md_raw
-        unless m.nil?
-          md = md_raw.gsub(pattern, "\n\n" + img_comments[m[:url]].map { |c| "- #{c}" }.join("\n"))
-        end
-        md
-      }
+      .map { |h| "### #{h.username} at [#{h.created_at.strftime('%T')}](#{h.permalink})\n#{format_body(h.text, file_comments)}" }
       .join("\n\n\n")
 
     category = target_date.strftime(ESA_REPORT_CATEGORY)
