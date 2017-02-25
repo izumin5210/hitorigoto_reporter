1dba0ece390c360e5cf5930267db2b2af80d2a9d
izumin5210 <masayuki@izumin.info>
2017-02-25 23:26:59 +0900

    Support paging

--- lib/hitorigoto_reporter/hitorigoto.rb
+++ lib/hitorigoto_reporter/hitorigoto.rb
@@ -10,17 +10,38 @@ module HitorigotoReporter
       @stamps     = stamps
     end
 
-    def self.fetch(channel_name, target_date)
-      date_query = target_date.strftime("%Y-%m-%d")
-      query = ["on:#{date_query}", "in:#{channel_name}"].join(' ')
-      res = Slack.client.search_messages(query: query)
-      res['messages']['matches']
-        .select { |m| m['type'] == 'message' }
-        .map { |m|
-          reaction = Slack.client.reactions_get(channel: m['channel']['id'], timestamp: m['ts'])
-          stamps = (reaction['message']['reactions'] || []).map { |r| r['name'] }
-          Hitorigoto.new(m, stamps) 
-        }
+    class << self
+      def fetch(channel_name, target_date)
+        date_query = target_date.strftime("%Y-%m-%d")
+        query = ["on:#{date_query}", "in:#{channel_name}"].join(' ')
+        fetch_all(query: query)
+      end
+
+      private
+
+      def fetch_all(query:)
+        list = []
+        loop.with_index(1) do |_, i|
+          res = Slack.client.search_messages(query: query, page: i)
+          list += extract_higorigoto_list_from_res(res)
+          break if complete?(res)
+        end
+        list
+      end
+
+      def extract_higorigoto_list_from_res(res)
+        res['messages']['matches']
+          .select { |m| m['type'] == 'message' }
+          .map { |m|
+            reaction = Slack.client.reactions_get(channel: m['channel']['id'], timestamp: m['ts'])
+            stamps = (reaction['message']['reactions'] || []).map { |r| r['name'] }
+            Hitorigoto.new(m, stamps) 
+          }
+      end
+
+      def complete?(res)
+        res['messages']['paging'].tap { |paging| break paging['page'] >= paging['pages'] }
+      end
     end
   end
 end
